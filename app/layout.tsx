63a721db9219ef86b36108f8bafba53b0a6ef69d
Рина Верн <rinavern@Vern-Mac.local>
2026-06-13 20:00:33 +0300

    Hardcode Metrika counter 109826277

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,30 +48,35 @@ export const metadata: Metadata = {
   robots: { index: true, follow: true },
 }
 
-const YM_ID = process.env.NEXT_PUBLIC_YM_ID
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru" className={`${montserrat.variable} ${spaceGrotesk.variable}`}>
-      <head />
+      <head>
+        <Script id="metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            ym(109826277, "init", {
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true,
+              webvisor: true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/109826277" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
+      </head>
       <body className="bg-bg text-text antialiased">
         <div className="noise-overlay" aria-hidden="true" />
         {children}
         <MessengerWidget />
-
-        {/* Yandex.Metrika */}
-        {YM_ID && (
-          <Script id="ym-init" strategy="afterInteractive">
-            {`
-              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-              ym(${YM_ID}, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true });
-            `}
-          </Script>
-        )}
       </body>
     </html>
   )
